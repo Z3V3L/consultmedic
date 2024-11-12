a9c102b0273fb383fc1db86bd61805984027f01e
Z3V3L <zevelbs@gmail.com>
2024-11-12 14:18:07 -0600

    Subida de archivos

--- subirArchivo.php
+++ subirArchivo.php
@@ -4,13 +4,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
-    <title>Consultmedic</title>
+    <title>Subir archivo | Consultmedic</title>
     <link rel="icon" type="image/x-icon" href="styles/logo_flat.png">
 
 
     <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/product/">
-
-    
 
     <!-- Bootstrap core CSS -->
     <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -24,6 +22,24 @@
 <?php include 'header-footer/header.php';?>
 
 <main>
+
+<div class="container">
+  <form action="upload.php" method="post" enctype="multipart/form-data">
+    <h3 class="my-3">Subir una receta o diagnóstico previo </h3>  
+    <div class="row" >
+      <div class="col">
+        <input type="file" class="form-control mt-2" id="inputSubirArchivo" name="fileToUpload">
+      </div>
+    </div>
+    <div class="row justify-content-end">
+      <div class="col-2 mt-3">
+        <button class="btn btn-outline-secondary" type="submit" value="Upload Image" name="submit">Subir archivo</button>
+      </div>
+    </div>
+  </form>
+</div>
+
+
 
 </main>
 
